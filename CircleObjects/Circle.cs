915e3ba3351fbede29cbdb99b79a8ab450ccd7c8
Kyra Moore <kyra.moore21@yahoo.com>
2024-05-10 16:29:09 -0400

    first commit

---
+++ CircleObjects/Circle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleObjects
+{
+    public class Circle
+    {
+        //properties
+        private double Radius { get; set; }
+
+        //constructor 
+        public Circle(double _radius) 
+        { 
+            Radius = _radius;
+        } 
+
+        //methods 
+        public double CalculateDiameter()
+        {
+            return 2 * Radius;
+        }
+        public double CalculateCircumference()
+        {
+            return 2 * Math.PI * Radius;
+        }
+        public double CalculateArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+        public void Grow()
+        {
+            Radius *= 2;
+        }
+        public double GetRadius()
+        {
+            return Radius;
+        }
+    }
+}
---
+++ CircleObjects/Program.cs
@@ -0,0 +1,59 @@
+﻿
+using CircleObjects;
+
+//maincode
+
+Circle myCircle = new Circle(Welcome());
+Console.WriteLine(Formulas(myCircle));
+
+
+while (YesOrNo())
+{
+    Console.WriteLine("The circle is magically growing");
+    myCircle.Grow();
+    Console.WriteLine(Formulas(myCircle));
+   
+}
+    Console.WriteLine($"Goodbye! The circle's final radius is {myCircle.GetRadius()}");
+
+
+//methods
+static double Welcome() 
+{
+    Console.WriteLine($"*.*.*Magic Circle Grower*.*.*");
+    Console.Write("Enter Radius: ");
+    double input; 
+    while(!double.TryParse(Console.ReadLine(), out input)) {
+        Console.WriteLine("Must be a number. Try Again.");
+    }
+    return input;
+}
+
+static bool YesOrNo()
+{
+    while (true)
+    {
+        Console.WriteLine("Should we use a little magic and have your circle grow? y/n");
+        string choice = Console.ReadLine().Trim().ToLower();
+        if (choice == "y")
+        {
+            return true;
+        }
+        else if (choice == "n")
+        {
+            return false;
+        }
+        else
+        {
+            Console.WriteLine("Invalid Input. Please try again.");
+        }
+    }
+}
+
+
+static string Formulas(Circle circle)
+{
+    return $"Diameter: {circle.CalculateDiameter()}\n" +
+           $"Circumference: {circle.CalculateCircumference()}\n" +
+           $"Area: {circle.CalculateArea()}\n";
+}
